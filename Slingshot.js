d6974d72e2b1f90c8cd28a7d036c75e2ea87a977
Dhruv-2007 <71203997+Dhruv-2007@users.noreply.github.com>
2020-11-21 11:55:15 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,19 @@
+class Slingshot{
+   constructor(bodyA,bodyB){
+   var options = {
+       bodyA : bodyA,
+       bodyB : bodyB,
+       stiffness : 0.2,
+       length : 30
+    }
+    this.chain = Constraint.create(options);
+    World.add(world,this.chain);
+   } 
+
+   display(){
+       var pointA = this.chain.bodyA.position;
+       var pointB = this.chain.bodyB.position;
+       strokeWeight(4);
+       line(pointA.x,pointA.y,pointB.x,pointB.y);
+   }
+}
